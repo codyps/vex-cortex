38d7f93640301759e6294fff8cca94ee06d23021
John Mesmon <jmesmon@gmail.com>
2010-04-05 02:25:04 -0400

    fix indention

--- main.c
+++ main.c
@@ -81,51 +81,46 @@ int main(void)
 *******************************************************************************/
 void RCC_Configuration(void)
 {
-  /* RCC system reset(for debug purpose) */
-  RCC_DeInit();
+	/* RCC system reset(for debug purpose) */
+	RCC_DeInit();
 
-  /* Enable HSE */
-  RCC_HSEConfig(RCC_HSE_ON);
+	/* Enable HSE */
+	RCC_HSEConfig(RCC_HSE_ON);
 
-  /* Wait till HSE is ready */
-  HSEStartUpStatus = RCC_WaitForHSEStartUp();
+	/* Wait till HSE is ready */
+	HSEStartUpStatus = RCC_WaitForHSEStartUp();
 
-  if(HSEStartUpStatus == SUCCESS)
-  {
-    /* Enable Prefetch Buffer */
-    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+	if(HSEStartUpStatus == SUCCESS) {
+		/* Enable Prefetch Buffer */
+		FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
 
-    /* Flash 2 wait state */
-    FLASH_SetLatency(FLASH_Latency_2);
- 	
-    /* HCLK = SYSCLK */
-    RCC_HCLKConfig(RCC_SYSCLK_Div1); 
-  
-    /* PCLK2 = HCLK */
-    RCC_PCLK2Config(RCC_HCLK_Div1); 
+		/* Flash 2 wait state */
+		FLASH_SetLatency(FLASH_Latency_2);
 
-    /* PCLK1 = HCLK/2 */
-    RCC_PCLK1Config(RCC_HCLK_Div2);
+		/* HCLK = SYSCLK */
+		RCC_HCLKConfig(RCC_SYSCLK_Div1); 
 
-    /* PLLCLK = 8MHz * 9 = 72 MHz */
-    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+		/* PCLK2 = HCLK */
+		RCC_PCLK2Config(RCC_HCLK_Div1); 
 
-    /* Enable PLL */ 
-    RCC_PLLCmd(ENABLE);
+		/* PCLK1 = HCLK/2 */
+		RCC_PCLK1Config(RCC_HCLK_Div2);
 
-    /* Wait till PLL is ready */
-    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
-    {
-    }
+		/* PLLCLK = 8MHz * 9 = 72 MHz */
+		RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
 
-    /* Select PLL as system clock source */
-    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+		/* Enable PLL */ 
+		RCC_PLLCmd(ENABLE);
 
-    /* Wait till PLL is used as system clock source */
-    while(RCC_GetSYSCLKSource() != 0x08)
-    {
-    }
-  }
+		/* Wait till PLL is ready */
+		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+
+		/* Select PLL as system clock source */
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+		/* Wait till PLL is used as system clock source */
+		while(RCC_GetSYSCLKSource() != 0x08);
+	}
 }
 
 /*******************************************************************************
