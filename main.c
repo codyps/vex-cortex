6e4c5b00b7962066432dac649c907749487c3f2f
John Mesmon <jmesmon@gmail.com>
2010-04-10 00:40:25 -0400

    use new main.c

--- main.c
+++ main.c
@@ -1,178 +1,86 @@
-/******************** (C) COPYRIGHT 2007 STMicroelectronics ********************
-* File Name          : main.c
-* Author             : MCD Application Team
-* Version            : V1.0
-* Date               : 10/08/2007
-* Description        : Main program body
-********************************************************************************
-* THE PRESENT SOFTWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*******************************************************************************/
+#include "stm32f10x.h"
 
-/* Includes ------------------------------------------------------------------*/
-#include "stm32f10x_lib.h"
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-GPIO_InitTypeDef GPIO_InitStructure;
-ErrorStatus HSEStartUpStatus;
-
-/* Private function prototypes -----------------------------------------------*/
-void RCC_Configuration(void);
-void NVIC_Configuration(void);
-void Delay(vu32 nCount);
-
-/* Private functions ---------------------------------------------------------*/
-
-/*******************************************************************************
-* Function Name  : main
-* Description    : Main program.
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-int main(void)
-{
-#ifdef DEBUG
-  debug();
+#if defined(USE_STDPERIPH_DRIVER)
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_flash.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_spi.h"
 #endif
 
-  /* Configure the system clocks */
-  RCC_Configuration();
-    
-  /* NVIC Configuration */
-  NVIC_Configuration();
+/*
+  #define  USARTx                     USART1
+  #define  GPIOx                      GPIOA
+  #define  RCC_APB1Periph_USARTx      RCC_APB2Periph_USART1
+  #define  GPIO_RxPin                 GPIO_Pin_10
+  #define  GPIO_TxPin                 GPIO_Pin_9
+*/
+void init_usart1(void) {
+	USART_InitTypeDef USART_InitStructure;
 
-  /* Enable GPIOC clock */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-  
-  /* Configure PC.4 as Output push-pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-  while (1)
-  {
-    /* Turn on led connected to PC.4 pin */
-    GPIO_SetBits(GPIOC, GPIO_Pin_4);
-    /* Insert delay */
-    Delay(0xAFFFF);
-
-    /* Turn off led connected to PC.4 pin */
-    GPIO_ResetBits(GPIOC, GPIO_Pin_4);
-    /* Insert delay */
-    Delay(0xAFFFF);
-  }
+	USART_InitStructure.USART_BaudRate = 115200;     // 115200
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	
+	//TODO: enable1 usart clock
+	
+	//TODO: configure usart1 gpios
+	
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 }
 
-/*******************************************************************************
-* Function Name  : RCC_Configuration
-* Description    : Configures the different system clocks.
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void RCC_Configuration(void)
+void init_adc(void) {
+	// ADCCLK(max 14Mhz)
+	// XXX: IFI overclocked the ADC to 18MHz.
+	//  PCLK2 /6 = 12 + 2/3 MHz
+    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_ADCPRE)
+		| RCC_CFG_ADCPRE_DIV6;
+}
+
+void init_gpio(void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	//TODO: enable gpio clock.
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	GPIO_PinRemapConfig(
+		  GPIO_Remap_USART2 
+		| GPIO_PartialRemap2_TIM2
+		| GPIO_PartialRemap_USART3
+		| GPIO_FullRemap_TIM3
+		, ENABLE);
+}
+
+void main(void)
 {
-	/* RCC system reset(for debug purpose) */
-	RCC_DeInit();
+#ifdef DEBUG
+	debug();
+#endif
+	init_rcc();
 
-	/* Enable HSE */
-	RCC_HSEConfig(RCC_HSE_ON);
-
-	/* Wait till HSE is ready */
-	HSEStartUpStatus = RCC_WaitForHSEStartUp();
-
-	if(HSEStartUpStatus == SUCCESS) {
-		/* Enable Prefetch Buffer */
-		FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-
-		/* Flash 2 wait state */
-		FLASH_SetLatency(FLASH_Latency_2);
-
-		/* HCLK = SYSCLK */
-		RCC_HCLKConfig(RCC_SYSCLK_Div1); 
-
-		/* PCLK2 = HCLK */
-		RCC_PCLK2Config(RCC_HCLK_Div1); 
-
-		/* PCLK1 = HCLK/2 */
-		RCC_PCLK1Config(RCC_HCLK_Div2);
-
-		/* PLLCLK = 8MHz * 9 = 72 MHz */
-		RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
-
-		/* Enable PLL */ 
-		RCC_PLLCmd(ENABLE);
-
-		/* Wait till PLL is ready */
-		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-
-		/* Select PLL as system clock source */
-		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-
-		/* Wait till PLL is used as system clock source */
-		while(RCC_GetSYSCLKSource() != 0x08);
+	for(;;) {
 	}
 }
 
-/*******************************************************************************
-* Function Name  : NVIC_Configuration
-* Description    : Configures Vector Table base location.
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void NVIC_Configuration(void)
-{
-#ifdef  VECT_TAB_RAM  
-  /* Set the Vector Table base location at 0x20000000 */ 
-  NVIC_SetVectorTable(NVIC_VectTab_RAM, 0x0); 
-#else  /* VECT_TAB_FLASH  */
-  /* Set the Vector Table base location at 0x08000000 */ 
-  NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);   
-#endif
-}
-
-/*******************************************************************************
-* Function Name  : Delay
-* Description    : Inserts a delay time.
-* Input          : nCount: specifies the delay time length.
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void Delay(vu32 nCount)
-{
-  for(; nCount != 0; nCount--);
-}
-
 #ifdef  DEBUG
-/*******************************************************************************
-* Function Name  : assert_failed
-* Description    : Reports the name of the source file and the source line number
-*                  where the assert_param error has occurred.
-* Input          : - file: pointer to the source file name
-*                  - line: assert_param error line source number
-* Output         : None
-* Return         : None
-*******************************************************************************/
 void assert_failed(u8* file, u32 line)
 { 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {
-  }
+	/* Infinite loop */
+	for(;;){
+	}
 }
 #endif
-
-/******************* (C) COPYRIGHT 2007 STMicroelectronics *****END OF FILE****/
