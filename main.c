8528260fd838dcc51c142a18b6d5fc03a3d97fdb
John Mesmon <jmesmon@gmail.com>
2010-04-11 19:42:08 -0400

    GPIO init and USART init progressing.

--- main.c
+++ main.c
@@ -46,30 +46,92 @@ void adc_init(void)
 	//	| RCC_CFG_ADCPRE_DIV6;
 }
 
+void usart_init(void) {
+	/** USART1:
+	TX = PA9, RX = PA10
+	**/
+	/* GPIO: */
+	// TX = PA9 = AF push/pull =
+	//  mode = 11, CNF = 10.
+	GPIOA->CRH &= ~(GPIO_CRH_MODE9
+		| GPIO_CRH_CNF9);
+	GPIOA->CRH |= GPIO_CRH_MODE9_0
+		| GPIO_CRH_MODE9_1
+		| GPIO_CRH_CNF9_1;
+	
+	// RX = PA10 = Floating input.
+	// mode = 00, CNF = 01
+	GPIOA->CRH &= ~(GPIO_CRH_MODE10
+		| GPIO_CRH_CNF10);
+	GPIOA->CRH |= GPIO_CRH_CNF10_0;
+	
+	/* USART */
+	
+	
+	/** USART2: **/
+	/* REMAP */
+	// TX = PD5 , RX = PD6
+	// CK = PD7, CTS = PD3 , RTS = PD4
+	AFIO->MAPR |= AFIO_MAPR_USART2_REMAP;
+	
+	/* GPIO */
+	
+	/* USART */
+	
+	/** USART3: **/
+	/* REMAP */
+	// TX = PC10, RX = PC11
+	// CK = PC12, CTS = PB13, RTS = PB14
+	AFIO->MAPR &= ~AFIO_MAPR_USART3_REMAP;
+	AFIO->MAPR |= 
+		AFIO_MAPR_REMAP_PARTIALREMAP;
+		
+	/* GPIO */
+	
+	/* USART */
+}
+
 void gpio_init(void) {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	
-	//TODO: enable gpio clock.
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
+	// enable gpio clock.
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN
+		| RCC_APB2ENR_IOPBEN
+		| RCC_APB2ENR_IOPCEN
+		| RCC_APB2ENR_IOPDEN
+		| RCC_APB2ENR_IOPEEN
+		| RCC_APB2ENR_IOPFEN
+		| RCC_APB2ENR_IOPGEN;
 
-	GPIO_PinRemapConfig(
-		  GPIO_Remap_USART2 
-		| GPIO_PartialRemap2_TIM2
-		| GPIO_PartialRemap_USART3
-		| GPIO_FullRemap_TIM3
-		, ENABLE);
+	// Set all gpios to "analog", aka diabled
+	GPIOA->CRL = GPIOB->CRL 
+		= GPIOC->CRL = GPIOD->CRL
+		= GPIOE->CRL = GPIOF->CRL
+		= GPIOG->CRL = 0;
+	GPIOA->CRH = GPIOB->CRH
+		= GPIOC->CRH = GPIOD->CRH
+		= GPIOE->CRH = GPIOF->CRH
+		= GPIOG->CRH = 0;
+	
+	/** TIM2: **/
+	// ETR/CH1 = PA0 , CH2 = PA1
+	//     CH3 = PB10, CH4 = PB11
+	AFIO->MAPR &= ~AFIO_MAPR_TIM2_REMAP;
+	AFIO->MAPR |= 
+		AFIO_MAPR_TIM2_REMAP_PARTIALREMAP2;
+	
+	/** TIM3: **/
+	// CH1 = PC6, CH2 = PC7
+	// CH3 = PC8, CH4 = PC9
+	AFIO->MAPR &= ~AFIO_MAPR_TIM3_REMAP;
+	AFIO->MAPR |=
+		AFIO_MAPR_TIM3_REMAP_FULLREMAP;
 }
 
 __attribute__((noreturn)) void main(void)
 {
 	rcc_init();
+	gpio_init();
 
 	for(;;) {
 	}
