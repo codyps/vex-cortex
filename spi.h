f8e8940ba83cd6d8ac81ef22d2676b7176fbf35e
John Mesmon <jmesmon@gmail.com>
2010-04-18 00:23:26 -0400

    split out spi and add debug printing functions

---
+++ spi.h
@@ -0,0 +1,13 @@
+#ifndef SPI_H_
+#define SPI_H_
+
+void print_m2u(spi_packet_vex *m2u);
+void print_joystick(struct oi_data *oi);
+void spi_packet_init_m2u(spi_packet_vex *m2u);
+void spi_packet_init_u2m(spi_packet_vex *u2m);
+bool is_master_ready(void);
+void vex_spi_xfer(spi_packet_vex *m2u, spi_packet_vex *u2m);
+void vex_spi_process_packets(spi_packet_vex *m2u, spi_packet_vex *u2m);
+void spi_init(void);
+
+#endif
